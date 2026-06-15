f53c5001ee23cd7784786b76230ed4d108807c65
Lumikin <lucas.m.ribeiro11@aluno.senai.br>
2026-06-15 15:39:36 -0300

    :sparkles: feat: parando de apagar as tabelas no banco ao inicializar

--- src/server.js
+++ src/server.js
@@ -9,13 +9,12 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/", routes);
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+});
 
-initializeDatabase()
-  .then(() => {
-    app.listen(process.env.SERVER_PORT, () => {
-      console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
-    });
-  })
-  .catch(err => {
-    console.error("Erro ao inicializar o banco de dados:", err);
-  });
+// initializeDatabase()
+//   .then(() => {})
+//   .catch(err => {
+//     console.error("Erro ao inicializar o banco de dados:", err);
+//   });
